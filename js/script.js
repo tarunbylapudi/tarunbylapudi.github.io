1f22f0a0661e2dd7f826c0a322e1750787fdfd36
tarunbylapudi <92293379+tarunbylapudi@users.noreply.github.com>
2022-08-15 22:34:38 +0530

    enhansment

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
- //Profile count
+
+setTimeout(() => {
+    const profilevisits = document.getElementById('profilevisits');
+    profilevisits.style.display = 'hidden'; 
+  }, 1000);
+  
+
  function websiteVisits(response) {
-    document.querySelector("#visits").textContent = response.value;
+   // document.querySelector("#visits").textContent = response.value;
+
+    var CountFunction=setInterval(countAnimation,40)
+    let count=0;
+
+    function countAnimation(){
+        count++;
+        document.querySelector("#visits").textContent=count;
+
+        if (count==response.value) {
+
+            clearInterval(CountFunction)
+            
+        }
+
+    }
 }
